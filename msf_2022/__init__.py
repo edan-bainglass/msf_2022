6dd7f4e7dea7a38e9a90e77ed8d2b5875b1925e8
edan-bainglass <edan.bainglass@gmail.com>
2022-01-24 14:02:50 -0600

    Initial commit after CMS Cookiecutter creation, version 1.6

---
+++ msf_2022/__init__.py
@@ -0,0 +1,11 @@
+"""Demo project for MSF Bootcamp January 2022."""
+
+# Add imports here
+from .functions import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
